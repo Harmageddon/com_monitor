44820b201cdd70806f7ed2e3395522fbacb8ba19
zero-24 <zero-24@users.noreply.github.com>
2015-09-12 18:11:11 +0200

    clean lines at the end of the file really

--- admin/sql/uninstall/mysql/uninstall.sql
+++ admin/sql/uninstall/mysql/uninstall.sql
@@ -7,3 +7,4 @@ DROP TABLE IF EXISTS `#__monitor_issues`;
 DROP TABLE IF EXISTS `#__monitor_issue_classifications`;
 
 DROP TABLE IF EXISTS `#__monitor_comments`;
+
--- admin/sql/updates/mysql/0.0.6.sql
+++ admin/sql/updates/mysql/0.0.6.sql
@@ -1,1 +1,2 @@
 # Placeholder file for database changes for version 0.0.6
+
