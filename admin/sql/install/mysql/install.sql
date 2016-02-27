b52ed36ef0a8185a012d652b46c2bc98e5d0a0e3
Constantin Romankiewicz <constantin@zweiiconkram.de>
2016-02-27 08:15:22 +0100

    Added table for attachments.
    Not implemented yet.

--- admin/sql/install/mysql/install.sql
+++ admin/sql/install/mysql/install.sql
@@ -86,3 +86,13 @@ CREATE TABLE IF NOT EXISTS `#__monitor_unread_issues`(
 	FOREIGN KEY (`user_id`) REFERENCES `#__users`(`id`) ON DELETE CASCADE,
 	FOREIGN KEY (`comment_id`) REFERENCES `#__monitor_comments`(`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__monitor_attachments`(
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Attachment ID, primary key',
+	`issue_id` int(10) unsigned COMMENT 'ID of the issue where the attachment belongs.',
+	`comment_id` int(10) unsigned COMMENT 'ID of the comment where the attachment belongs.',
+	`path` varchar(400) NOT NULL COMMENT 'File path for the attachment. Relative to the base folder of the component in /media.',
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`issue_id`) REFERENCES `#__monitor_issues`(`id`) ON DELETE CASCADE,
+	FOREIGN KEY (`comment_id`) REFERENCES `#__monitor_comments`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
